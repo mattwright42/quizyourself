6a96e16092150206fe5e359f8b2d7a72cfdbdbf7
Matthew Wright <matthewbwright@gmail.com>
2022-10-13 10:55:45 -0700

    sets up FinalScreen component

---
+++ src/Components/FinalScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import FetchButton from './FetchButton/';
+
+function FinalScreen() {
+    const score = useSelector((state) => state.score)
+    
+    const dispatch = useDispatch()
+
+    const replay = () => {
+        dispatch({
+            type: 'SET_INDEX',
+            index: 0
+        })
+
+        dispatch({
+            type: 'SET_SCORE',
+            score: 0
+        })
+    }
+
+    const settings = () => {
+        dispatch({
+            type: 'SET_QUESITONS',
+            questions: []
+        })
+
+        dispatch({
+            type: 'SET_SCORE',
+            score: 0
+        })
+    }
+
+    return (
+        <div>
+            <h3>FINAL SCORE: {score}</h3>
+            <button onClick={replay}>Try again!</button>
+            <FetchButton text="Fetch new questions" />
+            <button onClick={settings}>Back to Quiz settings</button>
+        </div>
+    )
+}
+
+
+export default FinalScreen
